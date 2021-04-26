555281601bea3ec5a9be514c7fe8e8c654008126
Kelly <kmrose08@gmail.com>
2021-04-26 18:57:30 -0400

    updated files

--- app.js
+++ app.js
@@ -18,141 +18,69 @@
         });
       });
       
-    //   function optionChanged() {
-    //     show();
-    //   };
+      function optionChanged() {
+        show();
+      };
       
-    //   show();
+      show();
     
-    //   function show() {
-    //     d3.json('happiness.csv').then(data=>{
-    //       var selection = d3.select('select').node().value
+      function show() {
+        d3.csv('world-happiness-report-2021.csv').then(data=>{
+          var selection = d3.select('#selDataset').node().value
           
-    //       var meta = data.metadata.filter(obj=>obj.id == selection)[0];
-    //       var sample = data.samples.filter(obj=>obj.id == selection)[0];
+
+          var sample = data.filter(obj=>obj['Country name'] == selection)[0];
       
-    //       d3.select('.charts').html('');
-    //       Object.entries(meta).forEach(([key,val])=>{
-    //         d3.select('.container').append('h5').text(key.toUpperCase()+': '+val)
-    //       });
+          d3.select('.charts').html('');
+          Object.entries(sample).forEach(([key,val])=>{
+            d3.select('.container').append('h5').text(key.toUpperCase()+': '+val)
+          });
     
     
-    //       var data = [
-    //         {
-    //           x: names.slice(0,10).reverse(),
-    //           y: generosity.slice(0,10).reverse().map(y=>'OTU '+y),
-    //           type: 'bar',
-    //           orientation: 'h'
-    //         }
-    //       ];
-    //       Plotly.newPlot('bar', data);
+          var data = [
+            {
+              x: names.slice(0,10).reverse(),
+              y: generosity.slice(0,10).reverse().map(y=>'OTU '+y),
+              type: 'bar',
+              orientation: 'h'
+            }
+          ];
+          Plotly.newPlot('bar', data);
         
-    //     console.log(sample);
+        console.log(sample);
     
-    //     var trace1 = {
-    //       x: names,
-    //         y: generosity,
-    //         type: 'bubble',
-    //         mode: 'markers',
-    //       marker: {
-    //         size:  generosity
-    //       }
-    //     };
+        var trace1 = {
+          x: names,
+            y: generosity,
+            type: 'bubble',
+            mode: 'markers',
+          marker: {
+            size:  generosity
+          }
+        };
     
-    //     var data = [trace1];
+        var data = [trace1];
         
-    //     var layout = {
-    //       title: 'Marker Size',
-    //       showlegend: false,
-    //       height: 600,
-    //       width: 600
-    //     };
+        var layout = {
+          title: 'Marker Size',
+          showlegend: false,
+          height: 600,
+          width: 600
+        };
         
-    //     Plotly.newPlot('bubble', data);
+        Plotly.newPlot('bubble', data);
     
-    //     var data = [
-    //         {
-    //           x: names,
-    //           y: generosity,
-    //           type: 'bubble',
-    //           mode: 'markers',
-    //             marker: {
+        var data = [
+            {
+              x: names,
+              y: generosity,
+              type: 'bubble',
+              mode: 'markers',
+                marker: {
     
-    //             }}
-    //         ];
-    //     })
-    // }
+                }}
+            ];
+        })
+    }
     
 
-
-
-// d3.json("happiness.json").then(function (data) {
-//     data.names.forEach(name => {
-//       d3.select('select').append('option').text(name)
-//     });
-//   });
-  
-//   function optionChanged() {
-//     show();
-//   };
-  
-//   show();
-
-//   function show() {
-//     d3.json('happiness.json').then(data=>{
-//       var selection = d3.select('select').node().value
-      
-//       var meta = data.metadata.filter(obj=>obj.id == selection)[0];
-//       var sample = data.samples.filter(obj=>obj.id == selection)[0];
-  
-//       d3.select('.charts').html('');
-//       Object.entries(meta).forEach(([key,val])=>{
-//         d3.select('.container').append('h5').text(key.toUpperCase()+': '+val)
-//       });
-
-
-//       var data = [
-//         {
-//           x: names.slice(0,10).reverse(),
-//           y: generosity.slice(0,10).reverse().map(y=>'OTU '+y),
-//           type: 'bar',
-//           orientation: 'h'
-//         }
-//       ];
-//       Plotly.newPlot('bar', data);
-    
-//     console.log(sample);
-
-//     var trace1 = {
-//       x: names,
-//         y: generosity,
-//         type: 'bubble',
-//         mode: 'markers',
-//       marker: {
-//         size:  generosity
-//       }
-//     };
-
-//     var data = [trace1];
-    
-//     var layout = {
-//       title: 'Marker Size',
-//       showlegend: false,
-//       height: 600,
-//       width: 600
-//     };
-    
-//     Plotly.newPlot('bubble', data);
-
-//     var data = [
-//         {
-//           x: names,
-//           y: generosity,
-//           type: 'bubble',
-//           mode: 'markers',
-//             marker: {
-
-//             }}
-//         ];
-//     })
-// }
